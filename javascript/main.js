27d76a99187d96d3aa1ac4bfa44ab0ef551ecd78
Yamitzky <negiga@gmail.com>
2013-01-12 15:03:38 +0900

    動画データから、レッツももくろちゃーん

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,12 @@
+var canvas = undefined;
+
 function setLoader(visible) {
   var el = document.getElementById('loader');
   el.style.display = visible ? 'inline-block' : 'none';
 }
 
-function showButton() {
-  setLoader(true);
-  var detector;
-  var canvas = document.getElementById('canvas');
-  canvas.width = this.width;
-  canvas.height = this.height;
-  canvas.getContext('2d').drawImage(this,0,0);
-  new HAAR.Detector(haarcascade_mcs_nose).image(this).complete(function(){
+function detectAndDraw(image) {
+  new HAAR.Detector(haarcascade_mcs_nose).image(image).complete(function(){
     setLoader(false);
     alert(this.objects.length+" Objects found");
     var rect=this.objects[0];
@@ -24,7 +20,31 @@ function showButton() {
   }).detect(1, 1.1, 0.1, 1, true);
 };
 
+function showButton() {
+  setLoader(true);
+  canvas.width = this.width;
+  canvas.height = this.height;
+  canvas.getContext('2d').drawImage(this,0,0);
+  detectAndDraw(this);
+};
+
+function loadStream(stream) {
+  var video = document.getElementById('video');
+  video.src = webkitURL.createObjectURL(stream);
+  var _canvas = document.createElement('canvas');
+  setTimeout(function() {
+    _canvas.width = video.videoWidth;
+    _canvas.height = video.videoHeight;
+    _canvas.getContext('2d').drawImage(video, 0, 0);
+    var captured = new Image();
+    captured.src = _canvas.toDataURL();
+    captured.addEventListener('load', showButton);
+  }, 2000);
+};
+
 window.addEventListener('load', function() {
+  canvas = document.getElementById('canvas');
+
   var fileInput = document.getElementById('file');
   fileInput.addEventListener('change', function() {
     reader = new FileReader();
@@ -35,4 +55,7 @@ window.addEventListener('load', function() {
       image.addEventListener('load', showButton);
     });
   });
+  if (navigator.webkitGetUserMedia) {
+    navigator.webkitGetUserMedia({video:true}, loadStream);
+  }
 });
