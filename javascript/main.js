7dbc77ad7bdc5bcd005ba436cc5304ef926028b5
Yamitzky <negiga@gmail.com>
2013-01-12 12:07:59 +0900

    鼻検知

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+function showButton() {
+  var detector;
+  var canvas = document.getElementById('canvas');
+  canvas.width = this.width;
+  canvas.height = this.height;
+  canvas.getContext('2d').drawImage(this,0,0);
+  new HAAR.Detector(haarcascade_mcs_nose).image(this).complete(function(){
+    alert(this.objects.length+" Objects found");
+    var rect=this.objects[0];
+    var rect2 = this.objects[1];
+    var ctx=canvas.getContext('2d');
+    ctx.strokeStyle="rgba(0,0,0,1)";
+    ctx.strokeRect(rect.x,rect.y,rect.width,rect.height);
+    //ctx.strokeRect(rect2.x,rect2.y,rect2.width,rect2.height);
+  }).detect(1, 1.1, 0.1, 1, true);
+};
+
+window.addEventListener('load', function() {
+  var image = new Image();
+  image.src = '/image/kanako.jpg';
+  image.addEventListener('load', showButton);
+});
